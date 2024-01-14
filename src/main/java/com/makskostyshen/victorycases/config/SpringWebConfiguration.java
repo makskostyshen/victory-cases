0d96775ac96a2eceeb34e0af8fa0d0bf87ecbdfc
maks <maxxxymko2003@gmail.com>
2024-01-14 15:33:34 +0200

    vict-14: configure path for static resources

--- src/main/java/com/makskostyshen/victorycases/config/SpringWebConfiguration.java
+++ src/main/java/com/makskostyshen/victorycases/config/SpringWebConfiguration.java
@@ -9,6 +9,7 @@ public class SpringWebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/assets/");
     }
 }
